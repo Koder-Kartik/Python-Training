2255a85f86029d892cfa75629e16c67542043050
Kartik Jain <koderkartik@gmail.com>
2025-11-11 13:19:45 +0530

    Update list_intro.py

--- Class/lists/list_intro.py
+++ Class/lists/list_intro.py
@@ -9,3 +9,43 @@ lst3 = [[1, 2], [3, 4]]
 lst4 = [1, 'ramu', 24 , 1,24]
 
 print(lst4)
+
+print(lst)
+
+print(lst2)
+
+lst5 = ['x', 'y', 'z']
+
+print(lst5)
+
+print(emptyList)
+
+print(lst3)
+
+print(lst3[0])
+
+print(lst3[1])
+
+print(lst3[0][0])  # Accessing first element of first sublist
+
+print(lst3[0][1])  # Accessing second element of first sublist
+
+print(lst3[1][0])  # Accessing first element of second sublist
+
+print(lst5[0])  # Accessing first element
+
+print(lst5[1])  # Accessing second element
+
+print(lst5[2])  # Accessing third element
+
+print(lst2[0])  # Accessing first element
+
+print(lst2[1])  # Accessing second element
+
+print(lst2[2])  # Accessing third element
+
+print(lst2[3])  # Accessing fourth element
+
+print(lst[0])  # Accessing first element
+
+print(lst[1])  # Accessing second element
