959b9dc2e6708661307298d7171f070e88701f91
Kartik Jain <koderkartik@gmail.com>
2025-11-11 13:17:43 +0530

    Update list_length.py

--- Class/lists/list_length.py
+++ Class/lists/list_length.py
@@ -2,3 +2,20 @@ lst = ['one', 'two', 'three', 'four']
 
 #find length of a list
 print(len(lst))
+
+lst2 = [1, 2, 3, 4, 5, 6]
+
+print(len(lst2))
+
+lst3 = []
+
+print(len(lst3))
+
+lst4 = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+print(len(lst4))
+
+lst5 = ['x', 'y']
+
+print(len(lst5))
+
