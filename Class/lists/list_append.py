1a716ed29835b353884ea115d9855febacde6989
Kartik Jain <koderkartik@gmail.com>
2025-11-11 13:15:11 +0530

    Create list_append.py

---
+++ Class/lists/list_append.py
@@ -0,0 +1,71 @@
+lst = ['one', 'two', 'three', 'four']
+
+lst.append('five')
+
+print(lst)
+
+lst.append(['six', 'seven'])
+
+print(lst)
+
+lst2 = [1, 2, 3]
+
+lst2.append(4)
+
+print(lst2) 
+
+lst2.append([5,6])
+
+print(lst2)
+
+lst3 = []
+
+lst3.append('first')
+
+print(lst3)
+
+lst3.append(['second', 'third'])
+
+print(lst3)
+
+lst3.append(4)
+
+print(lst3)
+
+lst3.append([5,6,7])
+
+print(lst3)
+
+lst4 = [1, 2, 3]
+
+lst4.append(None)
+
+print(lst4)
+
+lst4.append([])
+
+print(lst4)
+
+lst4.append([4,5])
+
+print(lst4)
+
+lst4.append('end')
+
+print(lst4)
+
+
+lst5 = ['a', 'b']
+
+lst5.append('c')
+
+print(lst5)
+
+lst5.append(['d', 'e'])
+
+print(lst5)
+
+lst5.append(1)
+
+print(lst5)
+
