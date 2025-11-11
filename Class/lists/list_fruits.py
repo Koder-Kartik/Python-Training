34454a89021ff077da51a445bdbc865d34126051
Kartik Jain <koderkartik@gmail.com>
2025-11-11 14:01:34 +0530

    Create list_fruits.py

---
+++ Class/lists/list_fruits.py
@@ -0,0 +1,40 @@
+fruits = [ 'Dragon Fruit', 'Mango', 'Apple', 'Banana', 'Grapes' ]
+
+print(fruits[0])
+
+print(fruits[-1])
+
+fruits.append('Orange')
+
+print(fruits) 
+
+fruits.insert(2, 'Pineapple')
+
+print(fruits)
+
+fruits.remove('Mango')
+
+print(fruits)
+
+fruits.pop()
+
+print(fruits)
+
+print(len(fruits))
+
+print(fruits.index('Apple'))
+
+print(fruits.count('Banana'))
+
+print(fruits)
+
+for i in fruits:
+    print(i)
+
+for i in fruits:
+    print("I like", i)
+
+print(len(fruits))
+
+
+
