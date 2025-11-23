fc1e6f16a649c48d7ccba3a601604fa96672cb08
Kartik Jain <koderkartik@gmail.com>
2025-11-23 11:20:54 +0530

    Create W.Mathematical_Expression.py

---
+++ Class/Sheet#1/W.Mathematical_Expression.py
@@ -0,0 +1,16 @@
+A, S, B, _, C = input().split()
+A = int(A)
+B = int(B)
+C = int(C)
+# Actual correct result
+if S == '+':
+    result = A + B
+elif S == '-':
+    result = A - B
+else:  # S == '*'
+    result = A * B
+# Check correctness
+if result == C:
+    print("Yes")
+else:
+    print(result)
